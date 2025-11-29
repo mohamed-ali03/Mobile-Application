be41e7860c72ce37dfd97abf72f191157dbe617a
mohamed-ali03 <mohamedalif163@gmail.com>
2025-11-29 17:49:21 +0200

    Theme change feature

---
+++ dualtheme/lib/pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:dualtheme/components/box.dart';
+import 'package:dualtheme/components/mybutton.dart';
+import 'package:dualtheme/pages/theme_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: MyBox(
+          color: Theme.of(context).colorScheme.primary,
+          child: Mybutton(
+            color: Theme.of(context).colorScheme.secondary,
+            onTap: () {
+              Provider.of<ThemeProvider>(context, listen: false).toggelTheme();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
